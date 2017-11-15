1e1f6ffb9acef2baaddc665ff349ec6abf678f97
Mahendhar <mahendarreddy@live.com>
2017-11-16 00:41:58 +0530

    Search Bar Container

---
+++ src/containers/search_bar.js
@@ -0,0 +1,36 @@
+import React, {Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''}
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+       <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={(event) => this.onInputChange(event)}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
